18db32890166f3332f235afbbd569b88bc8d0f77
Mohammed Taha Arhzane <tahaarhzane1@gmail.com>
2024-09-29 22:39:38 +0200

    commit

--- variables.tf
+++ variables.tf
@@ -1,20 +1,19 @@
 variable "prefix" {
-  description = "A prefix used for all resources in this example"
-  default     = "geia"
-}
+     description = "A prefix used for all resources in this example"
+     default     = "geia"
+   }
 
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  default     = "East US"
-}
+   variable "location" {
+     description = "The Azure Region in which all resources in this example should be created."
+     default     = "East US"
+   }
 
-variable "sql_admin_username" {
-  description = "The administrator username of the SQL Server."
-  default     = "sqladmin"
-}
+   variable "sql_admin_username" {
+     description = "The administrator username of the SQL Server."
+     default     = "sqladmin"
+   }
 
-variable "alert_email" {
-  description = "Email address for budget alerts"
-  default = "tahaarhzane1@gmailcom"
-  type        = string
-}
+   variable "alert_email" {
+     description = "Email address for budget alerts"
+     type        = string
+   }
